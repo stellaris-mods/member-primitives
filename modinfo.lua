b13006666ee80d8cd482edfb9162dff3eba79354
folknor <folk@folk.wtf>
2017-09-22 21:43:10 +0200

    Bump to 1.8.0.

--- modinfo.lua
+++ modinfo.lua
@@ -4,7 +4,7 @@ return {
 	name = "(re)Member Primitives?",
 	tags = { "Gameplay", "Diplomacy" },
 	picture = "thumb.png",
-	supported_version = "1.6.*",
+	supported_version = "1.8.*",
 	remote_file_id = 787280885,
 	readme = "readme.md",
 	steambb = "steam.bbcode",
