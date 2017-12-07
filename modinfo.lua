17756c4968416c27bddd0db25ed74e34cc61eae6
Folk <vhaarr@gmail.com>
2017-12-07 17:25:43 +0100

    Set originalUploader, and bump to 1.9.x.

--- modinfo.lua
+++ modinfo.lua
@@ -4,8 +4,9 @@ return {
 	name = "(re)Member Primitives?",
 	tags = { "Gameplay", "Diplomacy" },
 	picture = "thumb.png",
-	supported_version = "1.8.*",
+	supported_version = "1.9.*",
 	remote_file_id = 787280885,
+	originalUploader = "folk",
 	readme = "readme.md",
 	steambb = "steam.bbcode",
 }
